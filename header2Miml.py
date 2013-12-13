06c0284bbdc6c62560129cf24e07ca28967cadd8
TheoH <Theo0x48@gmail.com>
2013-12-12 16:09:36 -0800

    Converted header2Miml argument handling to argparse

--- header2Miml.py
+++ header2Miml.py
@@ -1,45 +1,38 @@
 #!/usr/bin/python
 
-#	Assumptions:
-#		- only parses extern functions
-#		- init/final functions only detected with "initialize" and "finalize" in name
-#		- any non-init/final functions with "initialize" or "finalize" aren't included
-#		- only data types recognized are [const|unsigned] [int|char]
-#		- variable names not defined are replaced by ARG# variable name
+# 	Assumptions:
+# 		- only parses extern functions
+# 		- init/final functions only detected with "initialize" and "finalize" in name
+# 		- any non-init/final functions with "initialize" or "finalize" aren't included
+# 		- only data types recognized are [const|unsigned] [int|char]
+# 		- variable names not defined are replaced by ARG# variable name
 
 import sys, re
+import argparse
 
-if (len(sys.argv) < 2):
-	print "Error: Too few arguments."
-	sys.exit(-1)
-elif len(sys.argv) > 3:
-	print "Error: Too many arguments."
-	sys.exit(-1)
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('header', help='C header to convert to a MIML module')
+    argparser.add_argument('modulename', nargs='?', help='Name of the MIML module file. Defaults to Header name')
+    args = argparser.parse_args()
 
-inputfile = sys.argv[1]
-rpos = inputfile.rfind(".")
+    if not args.header.endswith('.h'):
+        print "Error: Input file not a .h file."
+        sys.exit(-1)
+	basename = args.header[:-2]
 
-if len(sys.argv) == 2:
-	outputfile = inputfile
-	outputfile += ".miml"
+    inputfile = basename + '.h'
+    objfile = basename + '.o'
 
-if str(inputfile[rpos:]) != ".h" and (rpos > 0):
-	print "Error: Input file not a .h file."
-	sys.exit(-1)
-elif rpos < 0:
-	objfile = inputfile + ".o"
-	inputfile += ".h"
-else:
-	objfile = inputfile[:rpos] + ".o"
+    if not args.modulename:
+        outputfile = basename + '.miml'
+    elif args.modulename.endswith('.miml'):
+        outputfile = args.modulename
+    else:
+        print "Error: Output file not a .miml file."
+        sys.exit(-1)
 
-if len(sys.argv) == 3:
-	outputfile = sys.argv[2]
-	rpos = outputfile.rfind(".");
-	if (str(outputfile[rpos:]) != ".miml") and (rpos > 0):
-		print "Error: Output file not a .miml file."
-		sys.exit(-1)
-	elif rpos < 0:
-		outputfile += ".miml"
+
 
 try:
 	f = open(inputfile, 'r')
@@ -60,12 +53,12 @@ cgConf = f.readlines()
 f.close()
 
 for item in cgConf:
-	match = re.match( r'[\s]*allowed_types: \[(.*)\][\s]*', item, re.M|re.I)
+	match = re.match(r'[\s]*allowed_types: \[(.*)\][\s]*', item, re.M | re.I)
 	if match:
 		allowedTypes = match.group(1)
-		allowedTypes = re.sub( r"\,[\s]*", "|", allowedTypes)
-		allowedTypes = re.sub( r"'", "", allowedTypes)
-		allowedTypes = re.sub( r"(const|unsigned)[\s]+", "", allowedTypes)
+		allowedTypes = re.sub(r"\,[\s]*", "|", allowedTypes)
+		allowedTypes = re.sub(r"'", "", allowedTypes)
+		allowedTypes = re.sub(r"(const|unsigned)[\s]+", "", allowedTypes)
 		break
 
 
@@ -87,16 +80,16 @@ def xstr(s):
     return str(s)
 
 for item in codeLines:
-	#match = re.match( r'[\s]*(extern[\s]+)?([\w]+[\s]+)([\w_-]+)[\s]*\((.*)\).*', item.strip('\n'), re.M|re.I)
-	match = re.match( r'[\s]*(extern[\s]+)?([\w]+[\s]+)([\w_-]+)[\s]*\((.*)\)[\s]*;[\s]*(\/\/[\s]*\[[\s]*miml[\s]*:[\s]*(init|final|sender|receiver)[\s]*\])?.*', item.strip('\n'), re.M|re.I)
+	# match = re.match( r'[\s]*(extern[\s]+)?([\w]+[\s]+)([\w_-]+)[\s]*\((.*)\).*', item.strip('\n'), re.M|re.I)
+	match = re.match(r'[\s]*(extern[\s]+)?([\w]+[\s]+)([\w_-]+)[\s]*\((.*)\)[\s]*;[\s]*(\/\/[\s]*\[[\s]*miml[\s]*:[\s]*(init|final|sender|receiver)[\s]*\])?.*', item.strip('\n'), re.M | re.I)
 	if match:
 		mimlType = match.group(6)
-		if(mimlType=="init"):
+		if(mimlType == "init"):
 			outputCodeInit += str(match.group(3)) + "();"
 			foundInit += 1
 			continue
 
-		if(mimlType=="final"):
+		if(mimlType == "final"):
 			outputCodeFinal += str(match.group(3)) + "();"
 			foundFinal += 1
 			continue
@@ -107,24 +100,24 @@ for item in codeLines:
 		argVals = ""
 		for item2 in content:
 			counter += 1
-			match2 = re.match( r'[\s]*((const|unsigned)[\s]+)?(' + allowedTypes + ')[\s]*(\*)?([\s]*([\w_-]+)[\s]*)?', item2, re.M|re.I)
+			match2 = re.match(r'[\s]*((const|unsigned)[\s]+)?(' + allowedTypes + ')[\s]*(\*)?([\s]*([\w_-]+)[\s]*)?', item2, re.M | re.I)
 			if match2:
 				argType = xstr(match2.group(1)) + xstr(match2.group(3)) + xstr(match2.group(4))
 				argName = match2.group(6)
 				if argName is None:
 					argName = "ARG" + str(counter)
-				
-				argVals += "  - [" +argName + ", " + argType.strip() + "]\n"
-		
+
+				argVals += "  - [" + argName + ", " + argType.strip() + "]\n"
+
 		funcName = str(match.group(3))
-		if (mimlType=="receiver"):
-			outputCodeReceivers += "  " + funcName  + ":\n" + argVals
+		if (mimlType == "receiver"):
+			outputCodeReceivers += "  " + funcName + ":\n" + argVals
 			foundReceivers += 1
-		elif (mimlType=="sender"):
-			outputCodeSenders += "  " + funcName  + ":\n" + argVals
+		elif (mimlType == "sender"):
+			outputCodeSenders += "  " + funcName + ":\n" + argVals
 			foundSenders += 1
 		else:
-			outputCodeUnknown += "  " + funcName  + ":\n" + argVals
+			outputCodeUnknown += "  " + funcName + ":\n" + argVals
 			foundUnknown += 1
 
 try:
@@ -141,18 +134,18 @@ else:
 	fout.write("\n")
 fout.write(outputCodeSenders + "\n")
 fout.write(outputCodeReceivers + "\n")
-#if foundUnknown:
-#	fout.write("# Functions that have not been designated as\n")
-#	fout.write("# senders or receivers. Sort them accordingly\n")
-#	fout.write("# and delete the [unknown:] header.\n")
-#	fout.write(outputCodeUnknown + "\n")
+# if foundUnknown:
+# 	fout.write("# Functions that have not been designated as\n")
+# 	fout.write("# senders or receivers. Sort them accordingly\n")
+# 	fout.write("# and delete the [unknown:] header.\n")
+# 	fout.write(outputCodeUnknown + "\n")
 fout.close()
 
 print "\n " + inputfile + "  ->  " + outputfile
 print "======================================================"
-#print " Init\tFinal\tSenders\t  Receivers   Unknown			 "
+# print " Init\tFinal\tSenders\t  Receivers   Unknown			 "
 print " Init\tFinal\tSenders\t  Receivers					 "
 print "------------------------------------------------------"
-#print " " + str(foundInit) + "\t" + str(foundFinal) + "\t" + str(foundSenders) + "\t  " + str(foundReceivers) + "\t      " + str(foundUnknown)
+# print " " + str(foundInit) + "\t" + str(foundFinal) + "\t" + str(foundSenders) + "\t  " + str(foundReceivers) + "\t      " + str(foundUnknown)
 print " " + str(foundInit) + "\t" + str(foundFinal) + "\t" + str(foundSenders) + "\t  " + str(foundReceivers)
 print ""
