df110373a6dc7fef80b2e03e08ac0ebceb3c912a
TheoH <Theo0x48@gmail.com>
2014-11-28 03:43:59 -0800

    Cleaned up Expand
    Improved MimlCollector with help from Jamey
    Implemented some basic type checking on miml functions
    Made sure auto-generated args won't stomp on existing ones

--- elderberry/expand.py
+++ elderberry/expand.py
@@ -1,3 +1,5 @@
+import string
+import random
 import yaml
 from os import path
 import pycparser
@@ -9,43 +11,51 @@ from pycparserext.ext_c_generator import GnuCGenerator
 def plugin():
     return Expand
 
+
+class ExpandError(Exception):
+    pass
+
 class MimlCollector(c_ast.NodeVisitor):
     def __init__(self):
         self.miml_funcs=[]
 
-    def get_args(self, paramlist):
-        args = []
-        for node in paramlist.params:
-            pointers = []
-            while isinstance(node.type, c_ast.PtrDecl) or isinstance(node.type, c_ast.ArrayDecl):
-                pointers += ['*']
-                node = node.type
-            args += [[' '.join(node.type.type.names + pointers)]]
-            if node.type.declname:
-                args[-1].append(node.type.declname)
-        return args
+    @staticmethod
+    def get_arg(node):
+        pointers = []
+        while isinstance(node.type, (c_ast.PtrDecl, c_ast.ArrayDecl)):
+            pointers.append('*')
+            node = node.type
+        ctype = [' '.join(node.type.type.names + pointers)]
+        if node.type.declname:
+            ctype.append(node.type.declname)
+        return ctype
 
     def visit_Decl(self, node):
         for n in node.funcspec:
             if not isinstance(n, AttributeSpecifier):
                 continue
-            if not isinstance(n.exprlist.exprs[0], c_ast.FuncCall):
+            expr = n.exprlist.exprs[0]
+            if not isinstance(expr, c_ast.FuncCall):
                 continue
-            if n.exprlist.exprs[0].name.name != 'miml':
+            if expr.name.name != 'miml':
                 continue
-            args = self.get_args(node.type.args)
-            mimltype = n.exprlist.exprs[0].args.exprs[0].name
-            self.miml_funcs += [{'name': node.name,
-                                 'file': node.coord.file,
-                                 'type': mimltype,
-                                 'args': args
-                                }]
+
+            self.miml_funcs.append({
+                'name': node.name,
+                'file': node.coord.file,
+                'miml': expr.args.exprs[0].name,
+                'args': list(map(self.get_arg, node.type.args.params)),
+                'type': node.type.type.type.names
+            })
 
 
 class Expand:
-    def __init__(self, filename='', cppargs=''):
-        # TODO: warn if c func return isn't right for the type
-        self.cpp_args = cppargs.split() + [
+    def __init__(self, cppargs):
+        try:
+            self.cpp_args = cppargs.split()
+        except AttributeError:
+            self.cpp_args = []
+        self.cpp_args += [
             r'-DMIML_INIT=__attribute__((miml(init)))',
             r'-DMIML_FINAL=__attribute__((miml(final)))',
             r'-DMIML_SENDER=__attribute__((miml(sender)))',
@@ -65,29 +75,22 @@ class Expand:
             parsed = MimlCollector()
             parsed.visit(ast)
 
-            funcs = [func for func in parsed.miml_funcs if func.pop('file') == fullpath]
+            funcions = [func for func in parsed.miml_funcs if func['file'] == fullpath]
             basename = header.split('.')[0]
 
-            tree['modules'][basename] = {}
-            for func in funcs:
-                functype = func.pop('type')
-                #FIXME: if there are more than one init or final
-                if functype == 'init' or functype == 'final':
-                    tree['modules'][basename][functype] = func['name']
-                else:
-                    i = 0
-                    for arg in func['args']:
-                        if len(arg) == 1: #FIXME: don't stomp on already existing args
-                            arg.append('_arg'+str(i))
-                            i += 1
-                    try:
-                        tree['modules'][basename][functype][func['name']] = func['args']
-                    except KeyError:
-                        tree['modules'][basename][functype] = {func['name']: func['args']}
-            tree['modules'][basename]['object'] = basename + '.o'
-            tree['modules'][basename]['file'] = header
-            tree['modules'][basename]['path'] = pathname
-            tree['modules'][basename]['fullpath'] = fullpath
+            module = {}
+            for func in funcions:
+                if func['miml'] in ('init', 'final'):
+                    self.initfinal(module, func)
+                elif func['miml'] in ('sender', 'receiver'):
+                    self.sendreceive(module, func)
+
+            module['object'] = basename + '.o'
+            module['file'] = header
+            module['path'] = pathname
+            module['fullpath'] = fullpath
+
+            tree['modules'][basename] = module
 
     def findpath(self, filename):
         for pathname in self.includes:
@@ -96,4 +99,47 @@ class Expand:
                 return pathname
         else:
             raise IOError("Could not find header file: " + filename)
+
+    def initfinal(self, module, func):
+        # TODO: warn if init arg types are not right. Once I figure out what they are.
+        if func['type'] != ['void']:
+            raise ExpandError('Miml {} {} has incorrect return type ({})'.format(func['miml'], func['name'], func['type']))
+        if func['miml'] == 'final' and list(func['args']) != [['void']]:
+            raise ExpandError('Miml final function {} in {} has non-void arguments'.format(func['name'], func['file']))
+        if func['miml'] in module:
+            raise ExpandError('More than one miml {} specified for {}'.format(func['miml'], func['file']))
+        module[func['miml']] = func['name']
+
+    def sendreceive(self, module, func):
+        if func['type'] != ['void']:
+           raise ExpandError('Miml {} {} has incorrect return type ({})'.format(func['miml'], func['name'], func['type']))
+        argnames = []
+        for arg in func['args']:
+            if len(arg) == 2:
+                argnames.append(arg[1])
+
+        argname = self.uniqueArgName(argnames)
+        for i, arg in enumerate(func['args'], 1):
+            if len(arg) == 1:
+                arg.append(argname+str(i))
+
+        module.setdefault(func['miml'], {})[func['name']] = func['args']
+
+    @staticmethod
+    def uniqueArgName(arguments):
+    #Cantor's diagonalization. If our desired arg name is in the list of
+    #provided arguments, then construct a unique random string not in the
+    #list of arguments
+        if '_arg' in arguments:
+            unique = ''
+            for i, arg in enumerate(arguments):
+                try:
+                    char = arg[i]
+                except IndexError:
+                    char = ''
+                unique += random.choice(string.ascii_letters.replace(char, ''))
+            return unique
+        else:
+            return '_arg'
+
 
