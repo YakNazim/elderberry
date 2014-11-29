5651a2d07056d73f9897c43cad5b0dd1475b507a
TheoH <Theo0x48@gmail.com>
2014-11-29 00:40:06 -0800

    improved coverage of complicated.h

--- tests/data/complicated.h
+++ tests/data/complicated.h
@@ -2,9 +2,10 @@
 #define COMPLICATED_H_
 
 void normalFunc(void);
+void inline funcWithNonAttrFuncspec(void);
 void __attribute__((noreturn)) funcWithAttr(void);
 void __attribute__((nonnull(1))) funcWithFuncAttr(void *);
 
-void MIML_SENDER funcWithCollidingArgNames(int _arg1, int _arg2, int _arg3);
+void MIML_SENDER funcWithCollidingArgNames(int _arg1, int s, int _arg5);
 
 #endif
